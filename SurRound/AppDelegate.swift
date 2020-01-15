813c9b45e21bcb3f7dae42d1cd38821123f71754
charleshs <charlous167@gmail.com>
2020-01-15 19:59:43 +0800

    Create Xcode project

---
+++ SurRound/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  SurRound
+//
+//  Created by Kai-Ta Hsieh on 2020/1/15.
+//  Copyright © 2020 Kai-Ta Hsieh. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+    return true
+  }
+  
+}
+
---
+++ SurRound/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SurRound
+//
+//  Created by Kai-Ta Hsieh on 2020/1/15.
+//  Copyright © 2020 Kai-Ta Hsieh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+
+}
+
