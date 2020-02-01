ecd75c83e78718a9a356a913ebcf21d8ed03fe50
charleshs <charlous167@gmail.com>
2020-02-01 20:27:56 +0800

    Build UI of following list in explore page.

--- SurRound/Controller/Explore/FollowingListViewController.swift
+++ SurRound/Controller/Explore/FollowingListViewController.swift
@@ -10,8 +10,43 @@ import UIKit
 
 class FollowingListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = .yellow
+        
+        tableView.registerCellWithNib(withCellClass: ImagePostListCell.self)
+    }
+}
+
+extension FollowingListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: ImagePostListCell.identifier,
+            for: indexPath)
+        
+        return cell
+    }
+}
+
+extension FollowingListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return 400
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return UITableView.automaticDimension
     }
 }
---
+++ SurRound/View/Explore/ImagePostListCell.swift
@@ -0,0 +1,46 @@
+//
+//  ImagePostListCell.swift
+//  SurRound
+//
+//  Created by Kai-Ta Hsieh on 2020/2/1.
+//  Copyright © 2020 Kai-Ta Hsieh. All rights reserved.
+//
+
+import UIKit
+
+class ImagePostListCell: UITableViewCell {
+    
+    @IBOutlet weak var userImageView: UIImageView!
+    
+    @IBOutlet weak var postImageView: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var placeNameLabel: UILabel!
+    
+    @IBOutlet weak var likedButton: UIButton!
+    
+    @IBOutlet weak var likedCountLabel: UILabel!
+    
+    static var identifier: String {
+        return String(describing: ImagePostListCell.self)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        configureCell()
+    }
+    
+    // MARK: - User Actions
+    @IBAction func didTapLikedButton(_ sender: UIButton) {
+        
+    }
+    
+    
+    // MARK: - Private Methods
+    private func configureCell() {
+        
+        selectionStyle = .none
+    }
+}
